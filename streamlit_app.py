bb9da8138017e8cd3631a13f62155e3435ae27a2
Brian Flores <brian.flores@redica.com>
2024-08-07 08:28:25 -0600

    update app

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,34 +2,27 @@ import streamlit as st
 import pandas as pd
 
 # Título de la aplicación
-st.title('Aplicación de Recetas')
+st.title('Recetas')
 
 # Crear un input para subir un archivo
-uploaded_file = st.file_uploader("Sube tu archivo Excel", type=["xlsx"])
+uploaded_file = st.file_uploader("Excel de recetas", type=["xlsx"])
 
 # Crear un input para ingresar palabras a excluir
-exclude_words = st.text_area("Palabras a excluir de los ingredientes (separadas por comas)")
+selected_exclude_words = st.text_area("Palabras a excluir de los ingredientes (separadas por comas)")
+exclude_words = selected_exclude_words.lower().strip().split(",")
+exclude_words = [word.strip() for word in exclude_words if len(word.strip()) > 0]
 
 exclude_options = ['lechuga', 'pollo', 'pescado', 'pasta', 'queso', 'aceite', 'ajo', 'limón', 'huevos', 'espinacas']
-exclude_ingredients = st.multiselect("Palabras a excluir de los ingredientes", exclude_options)
+exclude_ingredients = st.multiselect("Ingredientes a excluir", exclude_options)
+exclude_list = exclude_words + exclude_ingredients
 
-
-#exclude_ingredients = [exclude_options[i] for i in selected_exclude_ingredients]
-#st.write(exclude_ingredients[0])
 
 if uploaded_file is not None:
     # Leer el archivo Excel
     df = pd.read_excel(uploaded_file)
 
-    if exclude_words:
-        # Convertir las palabras del text area en una lista
-        text_area_exclude_list = [word.strip() for word in exclude_words.split(",") if word.strip()]
-
-        # Combinar ambas listas de exclusión
+    if len(exclude_list) > 0:
         
-        exclude_list = text_area_exclude_list + exclude_ingredients
-        st.write(exclude_list)
-
         # Función para verificar si algún ingrediente contiene alguna de las palabras a excluir
         def should_exclude_recipe(ingredients, exclude_list):
             for word in exclude_list:
